a0cbd209dd78dd95208f4ae1744308efe58db2f0
Artyom Gaibovich <artyomgyubov2002@gmail.com>
2025-07-22 05:47:53 +0900

    TGAUTOMATIONCHANNELS-1: Добавлен модуль телеграм, реализованы CRUD ручки для категорий, пользователей, и каналов

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { UsersModule } from './modules/users/users.module';
+import { UserChannelsModule } from './modules/user-channels/user-channels.module';
+import { TelegramClientModule } from './modules/telegram-client/telegram-client.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CategoriesModule,
+    UsersModule,
+    TelegramClientModule,
+    UserChannelsModule
+  ],
   controllers: [],
   providers: [],
 })
--- apps/backend/src/modules/users/users.controller.ts
+++ apps/backend/src/modules/users/users.controller.ts
@@ -1,24 +1,14 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Delete, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
+  /*  @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
-  }
+  }*/
 
   @Get()
   findAll() {
@@ -27,13 +17,15 @@ export class UsersController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    return this.usersService.findOne(id);
   }
 
+  /*
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
+*/
 
   @Delete(':id')
   remove(@Param('id') id: string) {
